41a8e0d522d9aa926e9dc3bbaebe911be2398c63
Andrii Bobrov <aquariusmaster@yandex.ua>
2021-05-07 18:39:12 +0300

    Done task 5: employee-profile

--- employee-profile/src/main/java/com/bobocode/model/Employee.java
+++ employee-profile/src/main/java/com/bobocode/model/Employee.java
@@ -1,8 +1,11 @@
 package com.bobocode.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 /**
  * todo:
@@ -20,9 +23,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String fistName;
+    @Column(nullable = false)
     private String lastName;
 }
